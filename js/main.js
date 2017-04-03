05ee3e7b4ec8b0e36a30a20dc0d89e40208456b4
GasparColor <acl200@yandex.ru>
2017-04-03 19:30:15 +0500

    fix b1 b5 b8 b17 b28

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+var modal_open = document.querySelector(".featured-item__btn");
+var product_buy = document.querySelectorAll(".catalog-item__buy");
+
+var modal = document.querySelector(".modal-section");
+var modal_overlay = document.querySelector(".modal-section");
+
+
+if (modal_open) {
+    modal_open.addEventListener("click", function(event) {
+    event.preventDefault();
+    modal.classList.add("modal-open");
+  });
+};
+
+/*if (modal_overlay) {
+modal_overlay.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.classList.remove("modal-open");
+});
+};*/
+
+window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (modal.classList.contains("modal-open")) {
+            modal.classList.remove("modal-open");
+          }
+        }
+      });
+
+
+for (var i = 0; i < product_buy.length; i++) {
+    product_buy[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        if (modal) {
+            modal.classList.add("modal-open");
+        }
+    });
+}
+
  /*nav-*/
 
  var navMain = document.querySelector(".main-nav");
@@ -14,28 +53,3 @@
         navMain.classList.remove("main-nav--opened");
       }
     });
-
-
- /*modal*/
-
- 	var modalOpen = document.querySelector(".catalog-item__buy");
-
-  var modalPopup = document.querySelector(".modal-section");
-
-     modalOpen.addEventListener("click", function(event) {
-        event.preventDefault();
-        modalPopup.classList.add("modal-open");
-      });
-
-      modalClose.addEventListener("click", function(event) {
-        event.preventDefault();
-        modalPopup.classList.remove("modal-open");
-      });
-
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (modalPopup.classList.contains("modal-open")) {
-            modalPopup.classList.remove("modal-open");
-          }
-        }
-      });
